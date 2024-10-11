466cf57206689673bc1a15c43b8a23823cd7bcf4
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-10-12 00:15:16 +0300

    finsh the screen

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -1,23 +1,32 @@
 import 'colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:woodiex/core/theming/font_weight.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Fonts {
   static TextStyle gelasio24SemiBold = GoogleFonts.gelasio(
-    fontSize: 24,
-    fontWeight: FontWeight.w600,
+    fontSize: 24.sp,
+    fontWeight: FontWeightHelper.semiBold,
     color: ColorsManager.moreDarkGrey,
+    height: 1.25,
   );
 
   static TextStyle gelasio30Bold = GoogleFonts.gelasio(
-    fontSize: 30,
-    fontWeight: FontWeight.bold,
+    fontSize: 30.sp,
+    fontWeight: FontWeightHelper.bold,
     color: ColorsManager.darkGrey,
+    height: 1.27,
   );
 
   static TextStyle nunitoSansRegular18 = GoogleFonts.nunitoSans(
     fontSize: 18,
-    fontWeight: FontWeight.w400, // Regular
+    fontWeight: FontWeightHelper.regular, // Regular
     color: ColorsManager.secondaryGrey,
+  );
+  static TextStyle whiteGelasio18SemiBold = GoogleFonts.gelasio(
+    fontSize: 18.sp,
+    fontWeight: FontWeightHelper.semiBold,
+    color: ColorsManager.white,
   );
 }
--- lib/featrues/onboarding/onboarding_screen.dart
+++ lib/featrues/onboarding/onboarding_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:woodiex/core/theming/styles.dart';
+import 'package:woodiex/core/helpers/spacing.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:woodiex/featrues/onboarding/widgets/on_boarding_button.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -8,14 +12,36 @@ class OnboardingScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          // Background image, filling the screen
           Positioned.fill(
             child: Image.asset(
-              'assets/images/boarding.png', // Replace SVG with PNG image
+              'assets/images/boarding.png',
               fit: BoxFit.cover,
             ),
           ),
-          const Center(
-            child: Text('Welcome to the Onboarding Screen'),
+
+          Positioned.fill(
+            child: Padding(
+              padding: EdgeInsets.only(left: 30.w, top: 276.h),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'MAKE YOUR',
+                    style: Fonts.gelasio24SemiBold,
+                  ),
+                  verticalSpace(10),
+                  Text('HOME BEAUTIFUL', style: Fonts.gelasio30Bold),
+                  verticalSpace(35),
+                  Text(
+                    'The best simple place where you \n discover most wonderful furnitures\n and make your home beautiful',
+                    style: Fonts.nunitoSansRegular18,
+                  ),
+                  verticalSpace(200),
+                  Center(child: OnboardingButton())
+                ],
+              ),
+            ),
           ),
         ],
       ),
