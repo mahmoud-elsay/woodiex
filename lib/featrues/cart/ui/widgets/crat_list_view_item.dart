0e66bfaa20ab14b1ba58745d291c1147631efaa8
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-10-21 03:30:40 +0300

    working on cart

---
+++ lib/featrues/cart/ui/widgets/crat_list_view_item.dart
@@ -0,0 +1,103 @@
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter/material.dart';
+import 'package:woodiex/core/theming/colors.dart';
+import 'package:woodiex/core/theming/styles.dart';
+import 'package:woodiex/core/helpers/spacing.dart';
+import 'package:woodiex/core/theming/font_weight.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CartListViewItem extends StatefulWidget {
+  const CartListViewItem({super.key});
+
+  @override
+  State<CartListViewItem> createState() => _CartListViewItemState();
+}
+
+class _CartListViewItemState extends State<CartListViewItem> {
+  int quantity = 1;
+
+  void _increaseQuantity() {
+    setState(() {
+      quantity++;
+    });
+  }
+
+  void _decreaseQuantity() {
+    if (quantity > 1) {
+      setState(() {
+        quantity--;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              width: 100.w,
+              height: 100.h,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16.r),
+                child: Image.asset(
+                  'assets/images/prod.png',
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            horizontalSpace(30),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Coffee table',
+                  style: Fonts.darkGreyNunitoSansBold14.copyWith(
+                    fontWeight: FontWeightHelper.semiBold,
+                    color: ColorsManager.secondaryGrey,
+                  ),
+                ),
+                verticalSpace(10),
+                Text(
+                  '\$50',
+                  style: Fonts.darkGreyNunitoSansBold14.copyWith(
+                    color: ColorsManager.mainBlack,
+                    fontSize: 16.sp,
+                  ),
+                ),
+                verticalSpace(20),
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: _increaseQuantity,
+                      child: SvgPicture.asset('assets/svgs/add.svg'),
+                    ),
+                    horizontalSpace(5),
+                    Text(
+                      quantity.toString(),
+                      style: Fonts.darkGreyNunitoSansBold14,
+                    ),
+                    horizontalSpace(5),
+                    GestureDetector(
+                      onTap: _decreaseQuantity,
+                      child: SvgPicture.asset('assets/svgs/decrease.svg'),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const Spacer(),
+            SvgPicture.asset('assets/svgs/remove.svg'),
+          ],
+        ),
+        verticalSpace(10),
+        Divider(
+          height: 1.h,
+          color: Colors.grey.shade300,
+        ),
+        verticalSpace(10),
+      ],
+    );
+  }
+}
