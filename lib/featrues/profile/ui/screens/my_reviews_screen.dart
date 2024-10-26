d8b7aafccc972bf098c0930ece35a991baf12378
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-10-26 04:26:35 +0300

    strat my reviews screen

--- lib/featrues/profile/ui/screens/my_reviews_screen.dart
+++ lib/featrues/profile/ui/screens/my_reviews_screen.dart
@@ -1,10 +1,31 @@
+import 'package:flutter_svg/svg.dart';
 import 'package:flutter/material.dart';
+import 'package:woodiex/core/helpers/spacing.dart';
+import 'package:woodiex/core/widgets/backble_top_bar.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyReviewsScreen extends StatelessWidget {
   const MyReviewsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                BackableTopBar(screenTitle: 'My Reviews'),
+                Spacer(),
+                SvgPicture.asset('assets/svgs/search.svg'),
+              ],
+            ),
+            verticalSpace(20),
+          ],
+        ),
+      )),
+    );
   }
 }
