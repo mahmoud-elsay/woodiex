15ab98f3fe9ecd62a491c3f708bf1e6725a41259
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-10-27 14:02:10 +0300

    finsh profile module

--- lib/featrues/profile/ui/screens/setting_screen.dart
+++ lib/featrues/profile/ui/screens/setting_screen.dart
@@ -3,7 +3,9 @@ import 'package:woodiex/core/helpers/spacing.dart';
 import 'package:woodiex/core/widgets/backble_top_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:woodiex/featrues/profile/ui/widgets/settings_screen_widgets/password_info.dart';
+import 'package:woodiex/featrues/profile/ui/widgets/settings_screen_widgets/help_center_widget.dart';
 import 'package:woodiex/featrues/profile/ui/widgets/settings_screen_widgets/pesonal_info_widget.dart';
+import 'package:woodiex/featrues/profile/ui/widgets/settings_screen_widgets/notifications_widgets.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
@@ -22,6 +24,10 @@ class SettingScreen extends StatelessWidget {
               PersonalInfoWidget(),
               verticalSpace(35),
               PasswordInfo(),
+              verticalSpace(35),
+              NotificationsWidgets(),
+              verticalSpace(20),
+              HelpCenterWidget()
             ],
           ),
         ),
--- lib/featrues/profile/ui/widgets/settings_screen_widgets/custom_switch.dart
+++ lib/featrues/profile/ui/widgets/settings_screen_widgets/custom_switch.dart
@@ -13,41 +13,37 @@ class _CustomSwitchState extends State<CustomSwitch> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            setState(() {
-              isSwitched = !isSwitched;
-            });
-          },
-          child: AnimatedContainer(
-            duration: Duration(milliseconds: 200),
-            width: 40,
-            height: 24,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: isSwitched ? ColorsManager.green : Colors.grey[400],
-            ),
-            child: Stack(
-              children: [
-                AnimatedPositioned(
-                  duration: Duration(milliseconds: 200),
-                  curve: Curves.easeIn,
-                  left: isSwitched ? 16 : 0,
-                  right: isSwitched ? 0 : 16,
-                  child: Container(
-                    width: 24,
-                    height: 24,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                    ),
-                  ),
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isSwitched = !isSwitched;
+        });
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 200),
+        width: 40,
+        height: 24,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: isSwitched ? ColorsManager.green : Colors.grey[400],
+        ),
+        child: Stack(
+          children: [
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 200),
+              curve: Curves.easeIn,
+              left: isSwitched ? 16 : 0,
+              right: isSwitched ? 0 : 16,
+              child: Container(
+                width: 24,
+                height: 24,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
                 ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
