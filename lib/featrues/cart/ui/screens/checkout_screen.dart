a671d55beffb92c34b4e7e424686ee18921973bc
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-10-30 01:46:36 +0300

    checout done

--- lib/featrues/cart/ui/screens/checkout_screen.dart
+++ lib/featrues/cart/ui/screens/checkout_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:woodiex/core/theming/styles.dart';
 import 'package:woodiex/core/helpers/spacing.dart';
+import 'package:woodiex/core/widgets/app_text_button.dart';
 import 'package:woodiex/core/widgets/backble_top_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:woodiex/featrues/cart/ui/widgets/checkout_screen_widgets/info_card.dart';
+import 'package:woodiex/featrues/cart/ui/widgets/checkout_screen_widgets/order_details.dart';
 import 'package:woodiex/featrues/cart/ui/widgets/checkout_screen_widgets/shipping_address_for_checkout.dart';
 
 class CheckoutScreen extends StatelessWidget {
@@ -11,14 +15,38 @@ class CheckoutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
-        child: Column(
-          children: [
-            BackableTopBar(screenTitle: 'checkout'),
-            verticalSpace(15),
-            ShippingAddressForCheckout()
-          ],
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+          child: Column(
+            children: [
+              BackableTopBar(screenTitle: 'checkout'),
+              verticalSpace(15),
+              ShippingAddressForCheckout(),
+              verticalSpace(15),
+              InfoCard(
+                title: 'Payment',
+                iconPath: 'assets/svgs/visa_card.svg',
+                cardText: '**** **** **** 3947',
+                onEditTap: () {},
+              ),
+              verticalSpace(15),
+              InfoCard(
+                title: 'Delivery method',
+                iconPath: 'assets/svgs/dhl.svg',
+                cardText: 'Fast (2-3days)',
+                onEditTap: () {},
+              ),
+              verticalSpace(30),
+              OrderDetails(),
+              verticalSpace(50),
+              AppTextButton(
+                  buttonText: 'SUBMIT ORDER',
+                  textStyle:
+                      Fonts.whiteNunitoSansSemiBold18.copyWith(fontSize: 20.sp),
+                  onPressed: () {})
+            ],
+          ),
         ),
       )),
     );
---
+++ lib/featrues/cart/ui/widgets/checkout_screen_widgets/info_card.dart
@@ -0,0 +1,68 @@
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter/material.dart';
+import 'package:woodiex/core/theming/styles.dart';
+import 'package:woodiex/core/helpers/spacing.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class InfoCard extends StatelessWidget {
+  final String title;
+  final String iconPath;
+  final String cardText;
+  final VoidCallback? onEditTap;
+
+  const InfoCard({
+    super.key,
+    required this.title,
+    required this.iconPath,
+    required this.cardText,
+    this.onEditTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              title,
+              style: Fonts.secondaryGreyNunitoSansSemiBold18,
+            ),
+            const Spacer(),
+            GestureDetector(
+              onTap: onEditTap,
+              child: SvgPicture.asset('assets/svgs/edit_icon.svg'),
+            ),
+          ],
+        ),
+        verticalSpace(10),
+        Container(
+          width: 335.w,
+          height: 68.h,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.r),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 6,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              SvgPicture.asset(iconPath),
+              horizontalSpace(20),
+              Text(
+                cardText,
+                style: Fonts.darkNnitoSansSemiBold18.copyWith(fontSize: 14),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
