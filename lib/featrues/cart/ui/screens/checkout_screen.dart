4c3b44ccc3e4e5158a1b7b683696513e9287bd49
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-10-30 01:08:50 +0300

    fix navgation bug

--- lib/featrues/cart/ui/screens/checkout_screen.dart
+++ lib/featrues/cart/ui/screens/checkout_screen.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
-
+import 'package:woodiex/core/helpers/spacing.dart';
+import 'package:woodiex/core/widgets/backble_top_bar.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:woodiex/featrues/cart/ui/widgets/checkout_screen_widgets/shipping_address_for_checkout.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+        child: Column(
+          children: [
+            BackableTopBar(screenTitle: 'checkout'),
+            verticalSpace(15),
+            ShippingAddressForCheckout()
+          ],
+        ),
+      )),
+    );
   }
 }
---
+++ lib/featrues/cart/ui/widgets/checkout_screen_widgets/shipping_address_for_checkout.dart
@@ -0,0 +1,131 @@
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter/material.dart';
+import 'package:woodiex/core/theming/colors.dart';
+import 'package:woodiex/core/theming/styles.dart';
+import 'package:woodiex/core/helpers/spacing.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ShippingAddressForCheckout extends StatefulWidget {
+  const ShippingAddressForCheckout({super.key});
+
+  @override
+  State<ShippingAddressForCheckout> createState() =>
+      _ShippingAddressForCheckoutState();
+}
+
+class _ShippingAddressForCheckoutState
+    extends State<ShippingAddressForCheckout> {
+  bool _isEditing = false;
+  final TextEditingController _addressController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _addressController.text = 'Elmansora, Elmeena';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              'Shipping Address',
+              style: Fonts.secondaryGreyNunitoSansSemiBold18,
+            ),
+            Spacer(),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _isEditing = !_isEditing; // Toggle edit mode
+                });
+                // Log the edit icon click
+                debugPrint('Edit icon clicked');
+              },
+              child: SvgPicture.asset('assets/svgs/edit_icon.svg'),
+            ),
+          ],
+        ),
+        verticalSpace(5),
+        Container(
+          height: 127.h,
+          width: 335.w,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.r),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 6,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'mostfa naf3',
+                  style: Fonts.blacknNnitoSansBold18,
+                ),
+                verticalSpace(7),
+                Divider(
+                  color: Colors.grey.shade200,
+                  height: 0.3.h,
+                ),
+                _isEditing
+                    ? TextField(
+                        controller: _addressController,
+                        style: Fonts.nunitoSansRegular14,
+                        textAlign: TextAlign.left, // Align text to the left
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.r),
+                            borderSide: BorderSide(
+                              color: Colors.grey.shade300,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.r),
+                            borderSide: BorderSide(
+                              color: ColorsManager
+                                  .mainBlack, // Border color on focus
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.r),
+                            borderSide: BorderSide(
+                              color: Colors
+                                  .grey.shade300, // Border color when enabled
+                            ),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 10.h,
+                            horizontal: 10.w,
+                          ),
+                        ),
+                        onSubmitted: (value) {
+                          setState(() {
+                            _isEditing = false; // Exit edit mode
+                          });
+                        },
+                      )
+                    : Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          _addressController.text,
+                          style: Fonts.nunitoSansRegular14,
+                        ),
+                      ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
