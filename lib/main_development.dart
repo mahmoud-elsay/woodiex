5c1a2b4b343a3d7ce5c27fb1c9043aa211176fa5
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-05-12 07:33:32 +0300

    Refactor main entry points for development and production, update AndroidManifest and build.gradle for consistency

---
+++ lib/main_development.dart
@@ -0,0 +1,31 @@
+import 'package:woodiex/woodiex.dart';
+import 'package:flutter/material.dart';
+import 'package:woodiex/core/routing/routes.dart';
+import 'package:woodiex/core/routing/app_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:woodiex/core/helpers/shared_pref_helper.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
+  bool isFirstLaunch = await SharedPrefHelper.isFirstLaunch();
+  bool isLoggedIn = isFirstLaunch ? false : await checkIfLoggedInUser();
+
+  runApp(
+    ProviderScope(
+      child: Woodiex(
+        appRouter: AppRouter(),
+        initialRoute: isFirstLaunch
+            ? Routes.onBoardingScreen
+            : (isLoggedIn ? Routes.appLayout : Routes.loginScreen),
+      ),
+    ),
+  );
+}
+
+Future<bool> checkIfLoggedInUser() async {
+  String token = await SharedPrefHelper.getUserToken();
+  return token.isNotEmpty;
+}
