4cf6c29e79879a605447122ca5e130654aa4fb1a
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-06-12 09:56:50 +0300

    fix: reorder import statements in no_net_working_screen.dart for consistency

--- lib/core/screens/no_net_working_screen.dart
+++ lib/core/screens/no_net_working_screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:flutter/material.dart';
 import 'package:woodiex/core/theming/colors.dart';
 import 'package:woodiex/core/theming/styles.dart';
 
